2665a01858bb27deef89f5379f18ddfaceaff021
Feyza Karanfil Erat <56940562+feyzaerat@users.noreply.github.com>
2022-08-09 17:45:44 +0300

    Create Content.tsx

---
+++ src/components/Content.tsx
@@ -0,0 +1,19 @@
+
+import { DatePicker } from 'antd';
+import React, { Component } from 'react';
+
+import Card from './Elements/Card';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className='content'>
+                <h2>Content</h2>
+                <Card/>
+               
+            </div>
+        );
+    }
+}
+
+export default Content;
