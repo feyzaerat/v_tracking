3daeff555e91099c9cb54071da0fa521f28f967b
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-08-16 03:47:16 +0300

    modified header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import {Menu, MenuProps} from "antd";
+const items1: MenuProps['items'] = ['1', '2', '3', '4', '5'].map(key => ({
+  key,
+  label: `nav ${key}`,
+}));
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <h2 className='Header'>Header</h2>
-      </div>
+        <span><Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={items1} />
+        </span> </div>
     );
   }
 }
