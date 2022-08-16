ec0f589802a3c630d1b0dc1c18aed0d57d7c93f2
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-08-16 16:45:44 +0300

    class to hooks

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,40 @@
-import {Menu, MenuProps} from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined,HomeOutlined,UserOutlined,MenuOutlined, LogoutOutlined, LaptopOutlined, NotificationOutlined,} from '@ant-design/icons';
+import {Menu} from 'antd';
 import React from "react";
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-    (icon, index) => {
-        const key = String(index + 1);
+import {HomeOutlined, UserOutlined, ClockCircleOutlined, LogoutOutlined} from '@ant-design/icons';
 
-        return {
-            key: `sub${key}`,
-            icon: React.createElement(icon),
-            label: `subnav ${key}`,
+const Siders = () => {
+    return(
+        <>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={['2']}
+                items={[
+                    {
+                        key: '1',
+                        icon: <HomeOutlined />,
+                        label: 'Home',
+                    },
+                    {
+                        key: '2',
+                        icon: <ClockCircleOutlined />,
+                        label: 'Vehicles',
+                    },
+                    {
+                        key: '3',
+                        icon: <LogoutOutlined />,
+                        label: 'Logout',
+                    },
+                ]}
+            />
 
-            children: new Array(4).fill(null).map((_, j) => {
-                const subKey = index * 4 + j + 1;
-                return {
-                    key: subKey,
-                    label: `option${subKey}`,
-                };
-            }),
-        };
-    },
-);
+        </>
 
-const Siders = () => (
+    );
 
-    <Menu
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub2']}
-        style={{ height: '100%', borderRight: 0 }}
-        items={items2}
-    />
-);
+
+}
+
 
 export default Siders;
