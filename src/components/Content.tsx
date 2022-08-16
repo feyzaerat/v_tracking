e70a5282d1b3505ec34080e7a059c468aba1c5da
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-08-16 03:46:51 +0300

    added Breadcrumb

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,4 @@
-import { DatePicker } from 'antd';
+import {Breadcrumb, DatePicker} from 'antd';
 import React, { Component } from 'react';
 
 import Card from './Elements/Card';
@@ -7,7 +7,11 @@ class Content extends Component {
     render() {
         return (
             <div className='content'>
-
+                <Breadcrumb style={{ margin: '16px'}}>
+                    <Breadcrumb.Item>Vehicles</Breadcrumb.Item>
+                    <Breadcrumb.Item>Groups</Breadcrumb.Item>
+                    <Breadcrumb.Item>New Group</Breadcrumb.Item>
+                </Breadcrumb>
                 <Card/>
                
             </div>
