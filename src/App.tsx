ecaa84a0dd04a1e7bf059d7948344adad9850b2d
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-08-16 03:45:42 +0300

    modified layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Breadcrumb, Layout, Menu  } from 'antd';
+import type { MenuProps } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined,HomeOutlined,UserOutlined,MenuOutlined, LogoutOutlined, LaptopOutlined, NotificationOutlined,} from '@ant-design/icons';
 
 import './App.css';
 import Headers from './components/Header';
@@ -8,30 +10,34 @@ import Contents from './components/Content';
 import Footers from './components/Footer';
 const { Header, Footer, Sider, Content } = Layout;
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Layout className='Layout'>
-          <Sider>
-            <Sidebar />
-          </Sider>
-          <Layout>
-            <Header>
-              <Headers />
-            </Header>
 
-            <Content>
-              <Contents />
-            </Content>
+const App = () => {
+
+    return (
+        <>
+        <Layout>
+          <Header className="header">
+            <Headers />
+          </Header>
+            <Layout>
+
+              <Sider width={200} className="site-layout-background">
+               <Sidebar />
+              </Sider>
+              <Layout>
+
+                <Content className="site-layout-background" >
+                 <Contents />
+                </Content>
             <Footer>
               <Footers />
             </Footer>
           </Layout>
+             </Layout>
         </Layout>
-      </div>
+        </>
     );
-  }
+
 }
 
 export default App;
