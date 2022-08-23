539a6f66a83cb2590b798079a9f0f08ebc15969d
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-08-24 01:25:14 +0300

    .

--- src/components/Elements/Card.tsx
+++ src/components/Elements/Card.tsx
@@ -41,50 +41,28 @@ const Cards = () => {
     setstate({items: newArr});
   };
 
-
-    return (
-
+  return (
       <div>
-
-
-
         <Row gutter={24}>
           {state.items.map((tags, index)=>
-               <Col span={8}>
-                 <Form key={index} onSubmitCapture={handleSubmit}>
-
+             <Col span={8}>
+               <Form key={index} onSubmitCapture={handleSubmit}>
                   <div className="site-card-wrapper Card  text-center m-0-auto" >
-
-                                         <Card className='border-0 text-center' title="New Group" bordered={true} >
-
-
-                                            <Input style={{ width: 350 }} placeholder="Please Enter Group Name" /><br/>
-                                            <Selectbox/><br/>
-                                            <DatePicker/><DatePicker/><DatePicker/>
-
-                                           {state.items.length === 6 ? null :
-                                                <Button className=" gr-btn top-05vw" type="primary" onClick={add} style={{ width: 350 }}>+ Add New Group</Button>
-                                           }
-
-                                            <Button style={{ width: 350 }} title="Remove The Group" type="primary" danger className=' gr-btn top-05vw 'onClick={() => minus(tags.id)}>- Delete This Group</Button>
-                                           <button  style={{ width: 350 }} title="Save" type="submit"  className=' btn gr-btn top-05vw  success  '>Save</button>
-                                           </Card>
-
-                                   </div>
-
-                 </Form>
-               </Col> )}
+                    <Card className='border-0 text-center' title="New Group" bordered={true} >
+                      <Input style={{ width: 350 }} placeholder="Please Enter Group Name" /><br/>
+                      <Selectbox/><br/>
+                      <DatePicker/><DatePicker/><DatePicker/>
+                      {state.items.length === 6 ? null :
+                      <Button className=" gr-btn top-05vw" type="primary" onClick={add} style={{ width: 350 }}>+ Add New Group</Button>}
+                      <Button style={{ width: 350 }} title="Remove The Group" type="primary" danger className=' gr-btn top-05vw 'onClick={() => minus(tags.id)}>- Delete This Group</Button>
+                      <button  style={{ width: 350 }} title="Save" type="submit"  className=' btn gr-btn top-05vw  success  '>Save</button>
+                     </Card>
+                  </div>
+               </Form>
+             </Col> )}
         </Row>
-
-
-
-
-
-
       </div>
-
     )
-
 };
 
 export default Cards;
